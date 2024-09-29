1eacafbb44850f7896ad55bd020e0c627f71e510
Andy Ewing <aewing05@gmail.com>
2024-09-29 19:32:46 -0400

    Removing Google Sheets from Statement Analyzer

--- bank-statement-analyzer/dbconn.py
+++ bank-statement-analyzer/dbconn.py
@@ -1,18 +1,42 @@
 import pandas as pd
 import sqlite3
 
-
 def update_db(new_df):
+    table_exists = table_already_exists()
+
+    if table_exists:
+        print("table exists, pulling and merging data")
+        existing_df = get_existing_data()
+        insert_df = pd.concat(
+            [existing_df,
+             new_df[ ~new_df['transaction_id'].isin(existing_df['transaction_id'])],
+            ]
+        )
+ 
+    else:
+        print("table does not exist, adding data")
+        insert_df = new_df
+
     conn = sqlite3.connect('personal_finance.db')
-    existing_df = pd.read_sql_query("SELECT * from statement_transactions", conn)
+    insert_df.to_sql('statement_transactions', conn, if_exists='replace', index=False)
 
-    merged_df = pd.concat(
-        [existing_df,
-         new_df[ ~new_df['transaction_id'].isin(existing_df['transaction_id'])],
-        ]
-    )
 
-    merged_df.to_sql('statement_transactions', conn, if_exists='replace', index=False)
+def table_already_exists():
+    check_conn = sqlite3.connect('personal_finance.db')
+    cursor = check_conn.cursor()
+    cursor.execute("PRAGMA table_info(statement_transactions)")
+    result = cursor.fetchone()
+    check_conn.close()
 
-    conn.close()
+    if result is not None:
+        return True
+    else:
+        return False
+
+
+def get_existing_data():
+    read_conn = sqlite3.connect('personal_finance.db')
+    existing_df = pd.read_sql_query("SELECT * from statement_transactions", read_conn)
+    read_conn.close()
+    return existing_df
 
--- bank-statement-analyzer/run.py
+++ bank-statement-analyzer/run.py
@@ -6,20 +6,12 @@ import pdb
 import numpy as np
 import pandas as pd
 
-import gspread
-from gspread_dataframe import set_with_dataframe
-from google.oauth2.service_account import Credentials
 from os import listdir
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
 
 CSV_DIRECTORY = '/Users/aewing/Desktop/Dec-Jan Finances/'
-SHEET_KEY = '1HkcYF_-pmO9ZCDPQuGNzt7HOf_jfcL1FcfUifNN1FOs'
-WORKSHEET_NAME = 'raw_data'
 
 def main():
     aggregated_data = aggregate_data(CSV_DIRECTORY)
-    append_data_to_gsheet(aggregated_data, SHEET_KEY, WORKSHEET_NAME)
     update_db(aggregated_data)
     return
 
@@ -44,28 +36,6 @@ def aggregate_data(csv_directory):
     complete_data = complete_data[['transaction_id', 'date', 'description', 'category', 'card_used', 'credit', 'debit']]
     
     return complete_data
-
-def append_data_to_gsheet(data, sheet_key, worksheet_name):
-    scopes = ['https://www.googleapis.com/auth/spreadsheets',
-          'https://www.googleapis.com/auth/drive']
-
-    credentials = Credentials.from_service_account_file('../credentials.json', scopes=scopes)
-    gc = gspread.authorize(credentials)
-    
-    gauth = GoogleAuth()
-    drive = GoogleDrive(gauth)
-    
-    # open a google sheet
-    gs = gc.open_by_key(sheet_key)
-    # select a work sheet from its name
-    ws = gs.worksheet(worksheet_name)
-
-    data_list = data.fillna('').values.tolist()
-    if ws.acell('A1').value == None:
-        headers = data.columns.tolist()
-        data_list.insert(0, headers)
-
-    gs.values_append(worksheet_name, {'valueInputOption': 'RAW'}, {'values': data_list})
 
 if __name__ == '__main__':
     main()
