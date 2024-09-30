9f9b8eb0e818bdeadb18c411303fb5ef3e9db834
Andy Ewing <aewing05@gmail.com>
2024-09-29 20:38:18 -0400

    Adding Black and Formatting

--- bank-statement-analyzer/parsers.py
+++ bank-statement-analyzer/parsers.py
@@ -1,62 +1,108 @@
 import numpy as np
 import pandas as pd
 
+
 def parse_file(file):
-    if 'amex' in file:
+    if "amex" in file:
         return amex_csv_parser(file)
-    elif 'capitalone' in file:
+    elif "capitalone" in file:
         return capitalone_csv_parser(file)
-    elif 'chase' in file:
+    elif "chase" in file:
         return chase_csv_parser(file)
-    elif 'discover' in file:
+    elif "discover" in file:
         return discover_csv_parser(file)
-    elif 'schwab' in file:
+    elif "schwab" in file:
         return schwab_csv_parser(file)
-    elif 'nordstrom' in file:
+    elif "nordstrom" in file:
         return nordstrom_csv_parser(file)
     else:
         raise Exception("The CSV file could not be identified: ", file)
 
+
 def amex_csv_parser(file):
-    df = pd.read_csv(file).drop(columns=['Card Member', 'Account #'])
-    df['Debit'] = df['Amount'].astype(float).apply(lambda x: abs(x) if x < 0 else np.NaN)
-    df['Credit'] = df['Amount'].astype(float).apply(lambda x: abs(x) if x > 0 else np.NaN)
-    df['card_used'] = get_card_used(file, 'amex')
-    return df.drop(columns=['Amount'])
+    df = pd.read_csv(file).drop(columns=["Card Member", "Account #"])
+    df["Debit"] = (
+        df["Amount"].astype(float).apply(lambda x: abs(x) if x < 0 else np.NaN)
+    )
+    df["Credit"] = (
+        df["Amount"].astype(float).apply(lambda x: abs(x) if x > 0 else np.NaN)
+    )
+    df["card_used"] = get_card_used(file, "amex")
+    return df.drop(columns=["Amount"])
+
 
 def capitalone_csv_parser(file):
-    df = pd.read_csv(file).drop(columns=['Posted Date', 'Card No.', 'Category']).rename(columns={"Transaction Date": "Date"})
-    df['card_used'] = get_card_used(file, 'capitalone')
+    df = (
+        pd.read_csv(file)
+        .drop(columns=["Posted Date", "Card No.", "Category"])
+        .rename(columns={"Transaction Date": "Date"})
+    )
+    df["card_used"] = get_card_used(file, "capitalone")
     return df
+
 
 def chase_csv_parser(file):
-    df = pd.read_csv(file).drop(columns=['Post Date', 'Category', 'Type', 'Memo']).rename(columns={"Transaction Date": "Date"})
-    df['Debit'] = df['Amount'].astype(float).apply(lambda x: abs(x) if x < 0 else np.NaN)
-    df['Credit'] = df['Amount'].astype(float).apply(lambda x: abs(x) if x > 0 else np.NaN)
-    df['card_used'] = get_card_used(file, 'chase')
-    return df.drop(columns=['Amount'])
+    df = (
+        pd.read_csv(file)
+        .drop(columns=["Post Date", "Category", "Type", "Memo"])
+        .rename(columns={"Transaction Date": "Date"})
+    )
+    df["Debit"] = (
+        df["Amount"].astype(float).apply(lambda x: abs(x) if x < 0 else np.NaN)
+    )
+    df["Credit"] = (
+        df["Amount"].astype(float).apply(lambda x: abs(x) if x > 0 else np.NaN)
+    )
+    df["card_used"] = get_card_used(file, "chase")
+    return df.drop(columns=["Amount"])
 
-def discover_csv_parser(file):    
-    df = pd.read_csv(file).drop(columns=['Post Date', 'Category']).rename(columns={"Trans. Date": "Date"})
-    df['Debit'] = df['Amount'].astype(float).apply(lambda x: abs(x) if x < 0 else np.NaN)
-    df['Credit'] = df['Amount'].astype(float).apply(lambda x: abs(x) if x > 0 else np.NaN)
-    df['card_used'] = get_card_used(file, 'discover')
-    return df.drop(columns=['Amount'])
+
+def discover_csv_parser(file):
+    df = (
+        pd.read_csv(file)
+        .drop(columns=["Post Date", "Category"])
+        .rename(columns={"Trans. Date": "Date"})
+    )
+    df["Debit"] = (
+        df["Amount"].astype(float).apply(lambda x: abs(x) if x < 0 else np.NaN)
+    )
+    df["Credit"] = (
+        df["Amount"].astype(float).apply(lambda x: abs(x) if x > 0 else np.NaN)
+    )
+    df["card_used"] = get_card_used(file, "discover")
+    return df.drop(columns=["Amount"])
+
 
 def nordstrom_csv_parser(file):
-    df = pd.read_csv(file).rename(columns=lambda x: x.strip()).drop(columns=['Posting Date', 'Ref#']).rename(columns={"Transaction Date": "Date", "Transaction Detail": "Description"})
-    df['card_used'] = get_card_used(file, 'nordstrom')
-    df['Debit'] = df['Amount'].astype(float).apply(lambda x: abs(x) if x < 0 else np.NaN)
-    df['Credit'] = df['Amount'].astype(float).apply(lambda x: abs(x) if x > 0 else np.NaN)
-    df = df.drop(columns=['Amount'])
+    df = (
+        pd.read_csv(file)
+        .rename(columns=lambda x: x.strip())
+        .drop(columns=["Posting Date", "Ref#"])
+        .rename(
+            columns={"Transaction Date": "Date", "Transaction Detail": "Description"}
+        )
+    )
+    df["card_used"] = get_card_used(file, "nordstrom")
+    df["Debit"] = (
+        df["Amount"].astype(float).apply(lambda x: abs(x) if x < 0 else np.NaN)
+    )
+    df["Credit"] = (
+        df["Amount"].astype(float).apply(lambda x: abs(x) if x > 0 else np.NaN)
+    )
+    df = df.drop(columns=["Amount"])
     return df
+
 
 def schwab_csv_parser(file):
-    df = pd.read_csv(file).drop(columns=['Status', 'Type', 'CheckNumber', 'RunningBalance']).rename(columns={"Withdrawal": "Credit", "Deposit": "Debit"})
-    df['card_used'] = get_card_used(file, 'schwab')
+    df = (
+        pd.read_csv(file)
+        .drop(columns=["Status", "Type", "CheckNumber", "RunningBalance"])
+        .rename(columns={"Withdrawal": "Credit", "Deposit": "Debit"})
+    )
+    df["card_used"] = get_card_used(file, "schwab")
     return df
 
-def get_card_used(file, bank):
-    file = file.removesuffix('.csv')
-    return file[file.find(bank):]
 
+def get_card_used(file, bank):
+    file = file.removesuffix(".csv")
+    return file[file.find(bank) :]
--- bank-statement-analyzer/run.py
+++ bank-statement-analyzer/run.py
@@ -8,7 +8,8 @@ import pandas as pd
 
 from os import listdir
 
-CSV_DIRECTORY = '/Users/aewing/Desktop/Dec-Jan Finances/'
+CSV_DIRECTORY = "/Users/aewing/Desktop/Dec-Jan Finances/"
+
 
 def main():
     aggregated_data = aggregate_data(CSV_DIRECTORY)
@@ -16,11 +17,12 @@ def main():
     return
 
 
-def find_csv_filenames(path_to_dir, suffix=".csv" ):
+def find_csv_filenames(path_to_dir, suffix=".csv"):
     filenames = listdir(path_to_dir)
-    return [ filename for filename in filenames if filename.endswith( suffix ) ]
+    return [filename for filename in filenames if filename.endswith(suffix)]
 
-def aggregate_data(csv_directory): 
+
+def aggregate_data(csv_directory):
     filenames = find_csv_filenames(csv_directory)
     dfs = []
     for file in filenames:
@@ -29,13 +31,27 @@ def aggregate_data(csv_directory):
 
     complete_data = pd.concat(dfs, ignore_index=True)
     complete_data["category"] = np.nan
-    
-    complete_data["transaction_id"] = [hashlib.md5(str(x).encode('utf-8')).hexdigest() for x in complete_data.T.to_dict().values()]
+
+    complete_data["transaction_id"] = [
+        hashlib.md5(str(x).encode("utf-8")).hexdigest()
+        for x in complete_data.T.to_dict().values()
+    ]
 
     complete_data.columns = complete_data.columns.str.lower()
-    complete_data = complete_data[['transaction_id', 'date', 'description', 'category', 'card_used', 'credit', 'debit']]
-    
+    complete_data = complete_data[
+        [
+            "transaction_id",
+            "date",
+            "description",
+            "category",
+            "card_used",
+            "credit",
+            "debit",
+        ]
+    ]
+
     return complete_data
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
